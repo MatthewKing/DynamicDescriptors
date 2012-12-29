2f1ec39fa39bee2afe3532a88b1d6aec2106d9b0
Matthew King <mking@mking.info>
2012-12-29 23:48:36 +0800

    Initial functionality - initialization/factory.

---
+++ Source/DynamicDescriptors.Tests/DynamicDescriptorTests.cs
@@ -0,0 +1,27 @@
+﻿namespace DynamicDescriptors.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal sealed class DynamicDescriptorTests
+    {
+        [Test]
+        public void Create_InstanceIsNull_ThrowsArgumentNullException()
+        {
+            const string message = "instance should not be null.";
+            Assert.That(() => DynamicDescriptor.Create<object>(null),
+                Throws.TypeOf<ArgumentNullException>()
+                      .And.Message.Contains(message));
+        }
+
+        [Test]
+        public void Create_InstanceIsNotNull_ReturnsNewDynamicTypeDescriptorInstance()
+        {
+            object instance = new object();
+            DynamicTypeDescriptor<object> descriptor = DynamicDescriptor.Create(instance);
+
+            Assert.That(descriptor, Is.Not.Null);
+        }
+    }
+}
