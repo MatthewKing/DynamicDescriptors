1a15666c87154c44e58380b62fc7cb69bcc36a73
Matthew King <mking@mking.net>
2012-12-29 10:11:45 +0800

    Initial project structure.

---
+++ Source/AssemblyInfoShared.cs
@@ -0,0 +1,5 @@
+﻿[assembly: System.Reflection.AssemblyProduct("DynamicDescriptors")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © Matthew King 2012")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0")]
