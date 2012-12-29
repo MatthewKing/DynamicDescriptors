c50eeae784c005f94154499c16f5754a7c117476
Matthew King <mking@mking.net>
2012-12-29 23:48:36 +0800

    Initial functionality - initialization/factory.

---
+++ Source/DynamicDescriptors/DynamicDescriptor.cs
@@ -0,0 +1,37 @@
+﻿namespace DynamicDescriptors
+{
+    using System;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Facilitates the creation of custom type descriptors.
+    /// </summary>
+    public static class DynamicDescriptor
+    {
+        /// <summary>
+        /// Returns a new DynamicTypeDescriptor&lt;T&gt; instance that will supply dynamic
+        /// custom type information for the specified object.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the object for which dynamic custom type information will be supplied.
+        /// </typeparam>
+        /// <param name="instance">
+        /// The object for which dynamic custom type information will be supplied.
+        /// </param>
+        /// <returns>
+        /// A new DynamicTypeDescriptor&lt;T&gt; instance that will supply dynamic custom type
+        /// information for the specified object.
+        /// </returns>
+        public static DynamicTypeDescriptor<T> Create<T>(T instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance", "instance should not be null.");
+            }
+
+            TypeDescriptionProvider provider = TypeDescriptor.GetProvider(instance);
+            ICustomTypeDescriptor descriptor = provider.GetTypeDescriptor(instance);
+            return new DynamicTypeDescriptor<T>(descriptor);
+        }
+    }
+}
---
+++ Source/DynamicDescriptors/DynamicTypeDescriptor.cs
@@ -0,0 +1,20 @@
+﻿namespace DynamicDescriptors
+{
+    using System.ComponentModel;
+
+    /// <summary>
+    /// A runtime-customizable implementation of ICustomTypeDescriptor.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the object for which dynamic custom type information will be supplied.
+    /// </typeparam>
+    public sealed class DynamicTypeDescriptor<T> : CustomTypeDescriptor, ICustomTypeDescriptor
+    {
+        /// <summary>
+        /// Initializes a new instance of the DynamicTypeDescriptor class.
+        /// </summary>
+        /// <param name="parent">The parent custom type descriptor.</param>
+        public DynamicTypeDescriptor(ICustomTypeDescriptor parent)
+            : base(parent) { }
+    }
+}
