1245959910e55ccb6d61a5b98c001e8dacb49bda
Matthew King <mking@mking.info>
2012-12-29 10:11:45 +0800

    Initial project structure.

---
+++ Source/DynamicDescriptors/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿[assembly: System.Reflection.AssemblyTitle("DynamicDescriptors")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("296cb8f8-0e0b-4019-942e-ae309e5a3ec1")]
