d4d287fb823e45f9ff5d23bb20ba76c3507a942e
Matthew King <mking@mking.net>
2012-12-30 14:39:12 +0800

    Fleshing out dynamic property infrastructure.

--- Source/DynamicDescriptors/DynamicTypeDescriptor.cs
+++ Source/DynamicDescriptors/DynamicTypeDescriptor.cs
@@ -1,5 +1,7 @@
 ﻿namespace DynamicDescriptors
 {
+    using System;
+    using System.Collections.Generic;
     using System.ComponentModel;
 
     /// <summary>
@@ -11,10 +13,61 @@
     public sealed class DynamicTypeDescriptor<T> : CustomTypeDescriptor, ICustomTypeDescriptor
     {
         /// <summary>
+        /// A list containing the properties associated with this type descriptor.
+        /// </summary>
+        private readonly IList<DynamicPropertyDescriptor> dynamicProperties;
+
+        /// <summary>
         /// Initializes a new instance of the DynamicTypeDescriptor class.
         /// </summary>
         /// <param name="parent">The parent custom type descriptor.</param>
         public DynamicTypeDescriptor(ICustomTypeDescriptor parent)
-            : base(parent) { }
+            : base(Preconditions.CheckNotNull(parent, "parent"))
+        {
+            this.dynamicProperties = new List<DynamicPropertyDescriptor>();
+            foreach (PropertyDescriptor propertyDescriptor in base.GetProperties())
+            {
+                this.dynamicProperties.Add(new DynamicPropertyDescriptor(propertyDescriptor));
+            }
+        }
+
+        /// <summary>
+        /// Returns a collection of property descriptors for the object represented by this type
+        /// descriptor.
+        /// </summary>
+        /// <returns>
+        /// A PropertyDescriptorCollection containing the property descriptions for the object
+        /// represented by this type descriptor.
+        /// </returns>
+        public override PropertyDescriptorCollection GetProperties()
+        {
+            return this.GetProperties(null);
+        }
+
+        /// <summary>
+        /// Returns a collection of property descriptors for the object represented by this type
+        /// descriptor.
+        /// </summary>
+        /// <param name="attributes">
+        /// An array of attributes to use as a filter. This can be null.
+        /// </param>
+        /// <returns>
+        /// A PropertyDescriptorCollection containing the property descriptions for the object
+        /// represented by this type descriptor.
+        /// </returns>
+        public override PropertyDescriptorCollection GetProperties(Attribute[] attributes)
+        {
+            List<PropertyDescriptor> properties = new List<PropertyDescriptor>();
+
+            foreach (DynamicPropertyDescriptor property in this.dynamicProperties)
+            {
+                if (attributes == null || property.Attributes.Contains(attributes))
+                {
+                    properties.Add(property);
+                }
+            }
+
+            return new PropertyDescriptorCollection(properties.ToArray());
+        }
     }
 }
