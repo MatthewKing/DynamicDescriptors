5a0bdee30b556c22ae408ec3151c245d7fdf80bd
Matthew King <mking@mking.io>
2016-07-03 11:18:38 +0800

    Bump version number.

--- src/DynamicDescriptors.Tests/Properties/AssemblyInfo.cs
+++ src/DynamicDescriptors.Tests/Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DynamicDescriptors.Tests")]
 [assembly: AssemblyDescription("DynamicDescriptors unit tests")]
-[assembly: AssemblyCopyright("Copyright © Matthew King 2012-2015")]
+[assembly: AssemblyCopyright("Copyright © Matthew King 2012-2016")]
 [assembly: ComVisible(false)]
 [assembly: Guid("333be064-c9c2-4659-98a8-b1443ad713a7")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyInformationalVersion("1.3.1")]
